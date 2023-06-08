9e6631564f4a300f3d981b7c28e4c82d52d29bbb
Vladyslav Omelchenko <youchallengebread@gmail.com>
2023-06-08 19:43:02 +0200

    Added simple form validation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 class ToDo {
-    constructor(title, type) {
+    constructor(title, date, type) {
         this.title = title;
-        this.date = new Date().toLocaleDateString();
+        this.date = date;
         this.type = type;
     }
 }
-const user = new ToDo("take out trash", "Today");
-console.log(user);
+document.querySelector("#todo-add").addEventListener("click", () => {
+    const form = document.querySelector(".create-todo-container");
+    form.style.display = "block";
+});
+document.querySelector("#todo-close").addEventListener("click", () => {
+    const form = document.querySelector(".create-todo-container");
+    form.style.display = "none";
+});
+document.querySelector("#create-todo").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let title = document.getElementById("todo-title");
+    let date = document.getElementById("todo-date");
+    let type = document.getElementById("todo-type");
+
+    const todo = new ToDo(title.value, date.value, type.value);
+    console.log(todo);
+});
